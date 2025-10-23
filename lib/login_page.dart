c991f3e0268a677a8d150482bc2747ec2ae3838e
samchavita <ianchavarria00@gmail.com>
2025-10-24 00:05:27 +0800

    updated the login page

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -50,158 +50,147 @@ class LoginState extends State<LoginPage> {
       backgroundColor: Colors.blueGrey.shade50,
       appBar: AppBar(title: Text("Login")),
       body: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.only(top: 110.0),
-              child: Center(
-                child: Container(
-                  width: 200,
-                  height: 100,
-                  /*decoration: BoxDecoration(
-                        color: Colors.red,
-                        borderRadius: BorderRadius.circular(50.0)),*/
-                  // child: Image.asset('assets/images/Instagram.png'),
-                ),
-              ),
-            ),
-
-            Padding(
-              padding: const EdgeInsets.only(
-                left: 15.0,
-                right: 15.0,
-                top: 15,
-                bottom: 0,
-              ),
-              child: TextField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(60.0),
-                    borderSide: BorderSide(
-                      width: 5.0,
-                      color: Colors.grey.shade500,
-                    ),
+        child: Padding(
+          padding: const EdgeInsets.all(40.0),
+          child: Column(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(top: 110.0),
+                child: Center(
+                  child: Container(
+                    width: 200,
+                    height: 100,
+                    decoration: BoxDecoration(
+                          // color: Colors.red,
+                          borderRadius: BorderRadius.circular(50.0)),
+                    child: Image.asset('assets/images/pheasant.png'),
                   ),
-                  labelText: 'Email',
-                  hintText: 'example@gmail.com',
                 ),
               ),
-            ),
 
-            Padding(
-              padding: const EdgeInsets.only(
-                left: 15.0,
-                right: 15.0,
-                top: 15,
-                bottom: 0,
-              ),
-              child: TextField(
-                obscureText: true,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(60.0),
-                    borderSide: BorderSide(
-                      width: 5.0,
-                      color: Colors.grey.shade500,
+              Padding(
+                padding: const EdgeInsets.only(top: 15, bottom: 0),
+                child: TextField(
+                  decoration: InputDecoration(
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(60.0),
+                      borderSide: BorderSide(color: Colors.grey, width: 0.5),
                     ),
+                    labelText: 'Email',
+                    hintText: 'example@gmail.com',
                   ),
-                  labelText: 'Password',
-                  hintText: 'Enter secure password',
                 ),
               ),
-            ),
 
-            Padding(
-              padding: const EdgeInsets.only(
-                left: 15.0,
-                right: 15.0,
-                top: 15,
-                bottom: 0,
-              ),
-              child: SizedBox(
-                height: 45,
-                width: double.infinity,
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    foregroundColor: Colors.white,
-                    backgroundColor: Colors.teal,
-                  ),
-                  child: Text('Log in ', style: TextStyle(fontSize: 20)),
-                  onPressed: () {
-                    print('Successfully log in ');
-                  },
-                ),
-              ),
-            ),
-
-            Padding(
-              padding: const EdgeInsets.only(
-                left: 15.0,
-                right: 15.0,
-                top: 15,
-                bottom: 0,
-              ),
-              child: SizedBox(
-                width: double.infinity,
-                child: Row(
-                  children: <Widget>[
-                    Column(
-                      children: <Widget>[
-                        Row(
-                          children: <Widget>[
-                        Checkbox(
-                          value: true, // The current state of the checkbox
-                          onChanged: (bool? newValue) {
-                            // Callback when the checkbox is tapped
-                            // setState(() {
-                            //   _isChecked = newValue ?? false; // Update the state
-                            // });
-                          },
-                          activeColor: Colors.blue, // Color when checked
-                          checkColor: Colors.white, // Color of the checkmark
-                        ),
-                        Text('Remember me')
-                          ],
-                        ),
-                      ],
+              Padding(
+                padding: const EdgeInsets.only(top: 15, bottom: 0),
+                child: TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(60.0),
+                      borderSide: BorderSide(color: Colors.grey, width: 0.5),
                     ),
-                    Column(
-                      mainAxisAlignment:  MainAxisAlignment.end,
-                      children: <Widget>[
-                        Text('forgot password')
-                      ],
-                    ),
-                    
-                  ],
+                    labelText: 'Password',
+                    hintText: 'Enter secure password',
+                  ),
                 ),
               ),
-            ),
 
-            SizedBox(height: 50),
-            Center(
-              child: Row(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(left: 62),
-                    child: Text('Forgot your login details? '),
+              Padding(
+                padding: const EdgeInsets.only(top: 15, bottom: 0),
+                child: SizedBox(
+                  width: double.infinity,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Column(
+                        children: <Widget>[
+                          Row(
+                            children: <Widget>[
+                              Checkbox(
+                                value: _isChecked, // The current state of the checkbox
+                                onChanged: (bool? newValue) {
+                                  // Callback when the checkbox is tapped
+                                  setState(() {
+                                    _isChecked = newValue ?? false; // Update the state
+                                  });
+                                },
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(3.0),
+                                ),
+                                side: WidgetStateBorderSide.resolveWith(
+                                    (states) => BorderSide(color: Colors.grey, width: 0.5),
+                                ),
+                                activeColor: Colors.teal, // Color when checked
+                                checkColor:
+                                    Colors.white, // Color of the checkmark
+                              ),
+                              Text('Remember me',
+                                  style: TextStyle(fontSize: 12)
+                                  ),
+                            ],
+                          ),
+                        ],
+                      ),                      
+                      Column(
+                        children: <Widget>[
+                          Row(
+                            children: <Widget>[
+                              InkWell(
+                                onTap: () {
+                                  print('hello');
+                                },
+                                child: Text(
+                                  'Forgot password',
+                                  style: TextStyle(fontSize: 12, color: Colors.teal),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+
+                    ],
                   ),
+                ),
+              ),
 
-                  Padding(
-                    padding: const EdgeInsets.only(left: 1.0),
-                    child: InkWell(
+              Padding(
+                padding: const EdgeInsets.only(top: 15, bottom: 0),
+                child: SizedBox(
+                  height: 45,
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      foregroundColor: Colors.white,
+                      backgroundColor: Colors.teal,
+                    ),
+                    child: Text('Log in ', style: TextStyle(fontSize: 20)),
+                    onPressed: () {
+                      print('Successfully log in ');
+                    },
+                  ),
+                ),
+              ),
+
+              SizedBox(height: 40),
+              Center(
+                child:
+                    InkWell(
                       onTap: () {
                         print('hello');
                       },
                       child: Text(
-                        'Get help logging in.',
+                        'or sign up instead',
                         style: TextStyle(fontSize: 14, color: Colors.teal),
                       ),
                     ),
-                  ),
-                ],
+                    // ),
               ),
-            ),
-          ],
+              
+            ],
+          ),
         ),
       ),
     );
